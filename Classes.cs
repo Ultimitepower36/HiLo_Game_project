31ca1b4a84d0b4794de55d8f88f0be524b5c5234
Ultimitepower36 <113638764+Ultimitepower36@users.noreply.github.com>
2022-10-04 14:02:14 -0600

    starter code

---
+++ Classes.cs
@@ -0,0 +1,13 @@
+class Dealer {
+
+    List<int> spaces = new List<int>();
+
+    public List<int> Deck() {
+        spaces = new List<int> {1,2,3,4,5,6,7,8,9,10,11,12,13};
+        return spaces;
+    }
+
+    public void Checks() {
+
+    }
+}
