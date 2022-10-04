52cc8ceef206e172e5a851b0d670da276d6ead8b
Riley Larson <71296500+rslarson2019@users.noreply.github.com>
2022-10-04 14:16:22 -0600

    Comments Made

--- Program.cs
+++ Program.cs
@@ -5,7 +5,9 @@ Console.WriteLine("Welcome to Hilo!");
 
 class Game{
     static void Main(string[] args)
-    {
+    {   
+        // Call Dealer from Classes.cs to create Cards (Object)
+        // Define the Cards object as the new Dealer Class
         Dealer Cards = new Dealer();
         var ProtoDeck = Cards.Deck();
 
